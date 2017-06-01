9d7707c17c55bd891bc76c9f1807407fcdfa2310
Julian Nonino <noninojulian@outlook.com>
2017-05-31 22:17:39 -0300

    Adding two REST APIs to finish the process and the container

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
 This is a node.js app that serves a web page showing the ID of the docker container in which it is running.
 To build this app, from the root of this repository just run:  
   
-  docker build -t app1 app1
+    docker build -t app1 app1
 
 To create a container from that image, run:  
   
-  docker run -p XXXX:8080 app1
+    docker run -p XXXX:8080 app1
 
 You will be able to see the app going to:
   
-  localhost:XXXX
+    localhost:XXXX
--- app1/server.js
+++ app1/server.js
@@ -8,10 +8,21 @@ const PORT = 8080;
 
 // App
 const app = express();
+
 app.get('/', function (req, res) {
   res.send('<h1>Hello world from: ' + os.hostname() + '<h1>');
 });
 
+app.get('/exitWithError', function (req, res) {
+  res.send('Exit with Error');
+  process.exit(1);
+});
+
+app.get('/exitSuccess', function (req, res) {
+  res.send('Exit Successfully');
+  process.exit(0);
+});
+
+
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
-
